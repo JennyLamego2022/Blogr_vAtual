fdd08a471cfddfe6f090bf4985ce7318bc7f92ff
JennyLamego2022 <jennylamego@hotmail.com>
2023-01-13 20:30:24 -0300

    feat: função de abertura de submenu das demais listas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,25 +22,32 @@ export class HeaderComponent {
 
   clickSubMenuP(): void {
     const lista1 = document.getElementById("lista1");
+    const imgProduto = document.getElementById("imgProduto")
 
       if(lista1 !== null){
       lista1.classList.toggle("lista1-active")
+      imgProduto?.classList.toggle("imgProduto-active")
       }
 
   }
   clickSubMenuCa(): void {
     const lista2 = document.getElementById("lista2");
+    const imgCompanhia = document.getElementById("imgCompanhia")
 
       if(lista2 !== null){
       lista2.classList.toggle("lista2-active")
+      imgCompanhia?.classList.toggle("imgCompanhia-active")
       }
 
   }
   clickSubMenuCr(): void {
     const lista3 = document.getElementById("lista3");
+    const imgConectar = document.getElementById("imgConectar")
 
       if(lista3 !== null){
       lista3.classList.toggle("lista3-active")
+      imgConectar?.classList.toggle("imgConectar-active")
+
       }
 
   }
